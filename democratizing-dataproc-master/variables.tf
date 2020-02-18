55d60d8f46524ffe8c3f5435d95c78c5a580a920
kchanth <58708691+kchanth@users.noreply.github.com>
2020-02-18 13:45:10 -0800

    Add files via upload

---
+++ democratizing-dataproc-master/variables.tf
@@ -0,0 +1,28 @@
+variable "project" {
+  type = "string"
+}
+
+variable "region" {
+  type    = "string"
+  default = "europe-west1"
+}
+
+variable "database_version" {
+  description = "The database version."
+  default     = "MYSQL_5_7"
+}
+
+variable "zone" {
+  type    = "string"
+  default = "europe-west1-d"
+}
+
+variable "location" {
+  type    = "string"
+  default = "EU"
+}
+
+variable "storage_class" {
+  type    = "string"
+  default = "MULTI_REGIONAL"
+}
